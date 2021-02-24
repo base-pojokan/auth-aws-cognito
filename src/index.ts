cac6351fb4eb8d4cd0dfaa09e50230f11896b394
JuniYadi <juniyadi94@gmail.com>
2021-02-24 07:30:31 +0700

    Update Index Example

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,31 @@
-import * as aws from 'aws-sdk';
+import axios from 'axios';
+import * as dotenv from 'dotenv';
 
-export const authAWS = aws;
+// init dotenv
+dotenv.config();
+
+const cognitoIssuer : string = 'https://cognito-idp.{region}.amazonaws.com/{userPoolId}/.well-known/jwks.json';
+// const cognitoClaim : string = 'https://cognito-idp.us-east-1.amazonaws.com/<userpoolID>';
+
+/**
+ * Get Public Keys Default
+ */
+export const getPublicKeys = async () => {
+    try {
+        const request = await axios.get(cognitoIssuer);
+        const { data: response } = request;
+
+        return response;
+    } catch (e) {
+        throw e;
+    }
+}
+
+/**
+ * Validate AWS Cognito Token
+ * 
+ * @param token string
+ */
+export const validate = (token: String) => {
+    return token;
+}
